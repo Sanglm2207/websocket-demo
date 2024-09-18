deca57a11fa0aaa1133affe27f630a9313844640
SangLM <Sanglm2207@gmail.com>
2024-09-19 05:03:26 +0700

    first commit

--- src/main/java/org/github/websocket/controller/ChatController.java
+++ src/main/java/org/github/websocket/controller/ChatController.java
@@ -1,2 +1,25 @@
-package org.github.websocket.controller;public class ChatController {
+package org.github.websocket.controller;
+
+import org.github.websocket.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Message sendMessage(@Payload Message message) {
+        return message;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public Message addUser(@Payload Message message, SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        return message;
+    }
 }
--- src/main/java/org/github/websocket/model/Message.java
+++ src/main/java/org/github/websocket/model/Message.java
@@ -1,2 +1,17 @@
-package org.github.websocket.model;public class Message {
+package org.github.websocket.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+    private MessageType type;
+    private String content;
+    private String sender;
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
 }
